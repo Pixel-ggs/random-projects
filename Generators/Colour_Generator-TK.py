60e522f97d54dbe59562f09bfc8d83c7052d863a
Pixel-ggs <147253988+Pixel-ggs@users.noreply.github.com>
2026-09-17 17:23:55 +0100

    Add files via upload

---
+++ Generators/Colour_Generator-TK.py
@@ -0,0 +1,29 @@
+import tkinter as tk, random as r, time as t
+
+root = tk.Tk()
+root.geometry("300x200")
+
+R = r.randint(0, 225)
+G = r.randint(0, 225)
+B = r.randint(0, 225)
+
+def rgb(r, g, b):
+    return f'#{r:02x}{g:02x}{b:02x}'
+
+def gen():
+    R = r.randint(0, 225)
+    G = r.randint(0, 225)
+    B = r.randint(0, 225)
+    box.config(bg=rgb(R, G, B))
+    t.config(text=f"RGB Values: {R} ; {G} ; {B}")
+
+b = tk.Button(root, text="Generate", command=gen)
+b.pack(padx=20, pady=20)
+
+box = tk.Frame(root, width=60, height=40, bg=rgb(R, G, B))
+box.pack(padx=20, pady=20)
+
+t = tk.Label(root, text=f"RGB Values: {R} ; {G} ; {B}")
+t.pack()
+
+root.mainloop()
